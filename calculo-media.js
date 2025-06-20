b177c95ee1c68c3576afb97eeca0cbfb1a6be168
taitwelve <taynascimento15@gmail.com>
2025-06-20 09:53:34 -0300

    feat: add calculations for maximum, minimum, sum, and average of numbers

--- calculo-media.js
+++ calculo-media.js
@@ -6,3 +6,15 @@
 const notas = [10, 6.7, 9, 7.2];
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 console.log(`A média das notas é: ${media.toFixed(2)}`);
+
+//---------------------------------------------------------------------------
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+// utilizando array
+const numeros = [50, 43, 12];
+console.log(`O maior número é: ${Math.max(...numeros)}`);
+console.log(`O menor número é: ${Math.min(...numeros)}`);
+console.log(`A soma dos números é: ${numeros.reduce((acc, num) => acc + num, 0)}`);
+console.log(`A média dos números é: ${(numeros.reduce((acc, num) => acc + num, 0) / numeros.length).toFixed(0)}`);
+console.log(numeros);
