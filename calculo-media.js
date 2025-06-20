4d18ffd00117a693d42822e45d8a07b68f831f6d
taitwelve <taynascimento15@gmail.com>
2025-06-20 09:02:07 -0300

    refactor: update media calculation to use an array of grades

--- calculo-media.js
+++ calculo-media.js
@@ -3,5 +3,6 @@
 // const  nota3 = 8.5;
 // const nota4 = 7.2;
 
-const media = (nota1 + nota2 + nota3 + nota4) / 4;
+const notas = [10, 6.7, 9, 7.2];
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 console.log(`A média das notas é: ${media.toFixed(2)}`);
