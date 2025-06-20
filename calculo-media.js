a3de08bd0a7dd75237c021db6e1eae267dcd059e
taitwelve <taynascimento15@gmail.com>
2025-06-20 09:00:49 -0300

    comment out grade variables in calculo-media.js

--- calculo-media.js
+++ calculo-media.js
@@ -1,7 +1,7 @@
-const nota1 = 10;
-const nota2 = 6.7;
-const  nota3 = 8.5;
-const nota4 = 7.2;
+// const nota1 = 10;
+// const nota2 = 6.7;
+// const  nota3 = 8.5;
+// const nota4 = 7.2;
 
 const media = (nota1 + nota2 + nota3 + nota4) / 4;
 console.log(`A média das notas é: ${media.toFixed(2)}`);
