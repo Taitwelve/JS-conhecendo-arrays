f62a42fb7116c936e75a2bb05dda8cdc1e3f66d2
taitwelve <taynascimento15@gmail.com>
2025-06-25 13:02:41 -0300

    feat: add functionality to update student list with new entries

---
+++ atualizar-lista.js
@@ -0,0 +1,4 @@
+const listaEstudantes = ['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Carla'];
+listaEstudantes.splice(1, 2, 'Fernanda', 'Gustavo', 'Sofia');
+
+console.log('Lista atualizada:', listaEstudantes);
