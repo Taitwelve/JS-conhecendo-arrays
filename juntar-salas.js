fa53eda584f7ffc45f41acee1ae17236a85e5d6c
taitwelve <taynascimento15@gmail.com>
2025-06-25 13:08:00 -0300

    feat: add initial implementation to unify two classrooms into one array

---
+++ juntar-salas.js
@@ -0,0 +1,5 @@
+const salaJS = ['Evaldo', 'Camis', 'Leo'];
+const salaPython = ['Ju', 'Leo', 'Ana'];
+
+const salaUnificada = salaJS.concat(salaPython);
+console.log('Sala unificada:', salaUnificada);
