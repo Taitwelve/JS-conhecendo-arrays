e9d8f7c82318e976d3208c0b24db033712d8b5d5
Taitwelve <seuemail@exemplo.com>
2025-06-27 20:26:29 -0300

    fix: correct console log message to reflect past tense for average calculation

--- adicionar-nota.js
+++ adicionar-nota.js
@@ -1,4 +1,4 @@
 const notas = [10, 6.7, 9];
 notas.push(7.2);
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
-console.log(`A média das notas é: ${media.toFixed(2)}`);
+console.log(`A média das notas foi: ${media.toFixed(2)}`);
