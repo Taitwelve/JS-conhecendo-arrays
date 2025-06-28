a36f2e002d7214e96a8c6d3b9805f924b387709f
Taitwelve <seuemail@exemplo.com>
2025-06-28 11:54:38 -0300

    feat: add initial implementation of employee data structure with names, ages, and college status

---
+++ matrrizes.js
@@ -0,0 +1,8 @@
+const nomes = ["Ana", "Juliana", "Leonardo"];
+const idades = [30, 35, 28];
+const faculdade = [false, true, true];
+ 
+let funcionarios = [nomes, idades, faculdade];
+
+console.log(nomes);
+console.log(funcionarios[0][1]);
