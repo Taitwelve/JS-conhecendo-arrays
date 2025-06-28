085e0ac30a91b02163555d51c26440961064ce5b
Taitwelve <seuemail@exemplo.com>
2025-06-28 11:54:32 -0300

    refactor: add newline and improve code formatting in exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,3 +1,8 @@
 const lista = [7, 13, 'JavaScript', 'Alura', true];
 lista.push(false);
 console.log(lista.length);
+
+//-------------------------------------------------------------------------------------------------------
+
+const funcionarios = [["Ana", "Juliana", "Leonardo"],[30, 35, 28]];
+console.log(`${funcionarios[0][2]} tem ${funcionarios[1][2]} anos`)
