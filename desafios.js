0076f1297bb5d424ef172c60d205d558c08d8149
Taitwelve <seuemail@exemplo.com>
2025-06-28 12:06:08 -0300

    feat: add new array manipulations including slice, splice, concat, and create a 2D matrix

--- desafios.js
+++ desafios.js
@@ -57,3 +57,37 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const concatenatedArray = concatenateArrays(array1, array2);
 console.log(concatenatedArray); 
+
+//----------------------------------------------------------------------------------------------------------------------------
+//Crie um array chamado numeros contendo números de 1 a 10. Utilize o método slice para criar um novo array chamado parteNumeros que contenha apenas os números de índice 3 a 7 de numeros.
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const parteNumeros = numeros.slice(2, 7);
+console.log('Parte dos números: ', parteNumeros); 
+
+//----------------------------------------------------------------------------------------------------------------------------
+//Dado o array frutas contendo frutas que desejamos comprar na feira: const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi']Utilize o método splice para remover as frutas no índice 2 e 3 e, em seguida, adicione as frutas 'Kiwi' e 'Pêssego' nesses mesmos índices.
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+frutas.splice(2, 2, 'Kiwi', 'Pêssego');
+console.log(frutas);
+
+//----------------------------------------------------------------------------------------------------------------------------
+//Crie dois arrays chamados menuPrincipal e menuDeSobremesas contendo opções do cardápio de um restaurante. Utilize o método concat para criar um novo array menuCompleto contendo todos os elementos de menuPrincipal seguidos pelos elementos de menuDeSobremesas.
+const menuPrincipal = ['Arroz', 'Feijão', 'Carne', 'Frango'];
+const menuDeSobremesas = ['Bolo', 'Sorvete', 'Pudim'];
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas);
+console.log('Veja o menu completo:', menuCompleto );
+
+//----------------------------------------------------------------------------------------------------------------------------
+//Crie uma lista bidimensional com 3 linhas e 3 colunas, onde cada elemento seja uma matriz 3x3 com valores iniciando em 1 e aumentando em 1 a cada elemento.
+
+const matrizBidimensional = [];
+for (let i = 0; i < 3; i++) {
+    const linha = [];
+    for (let j = 0; j < 3; j++) {
+        linha.push(i * 3 + j + 1);
+    }
+    matrizBidimensional.push(linha);
+}
+console.log('Matriz de duas dimensões:');
+console.log(matrizBidimensional);
+matrizBidimensional.forEach(row => console.log(row));
