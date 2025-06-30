604e78570b95b0062d0ac81603c8170d91c68784
Taitwelve <seuemail@exemplo.com>
2025-06-30 12:42:03 -0300

    fix: exercicio de incremento

--- diferencadei++e++i.js
+++ diferencadei++e++i.js
@@ -5,3 +5,11 @@ console.log(i);
 let j = 8;
 console.log(++j); 
 console.log(++j);   
+
+// Exercícios de i++ e ++i
+let a = 5;
+let valorA = a++;
+let valorB = ++a;
+
+console.log("O valor de valorA é:", valorA);
+console.log("O valor de valorB é:", valorB);
