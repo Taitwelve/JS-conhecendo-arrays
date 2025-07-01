3fa15ab6f9ad229ec67ca07790d4878ef6c7ab1a
Taitwelve <seuemail@exemplo.com>
2025-07-01 15:25:56 -0300

    fix: exemplo de numeros de 0 a 10

--- for-classico.js
+++ for-classico.js
@@ -4,4 +4,13 @@ const notas = [10, 8.5, 7, 9.5, 6, 8, 10];
 //Terceira expressão é executada sempre ao final do bloco
 for (let indice = 0; indice < notas.length; indice++) {
     console.log(indice, notas[indice]);
+    console.log(`Nota ${indice + 1}: ${notas[indice]}`);
 }
+
+//---------------------------------------------------------------
+//Exemplo de for que imprime no terminal numeros de 0 a 10
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+//---------------------------------------------------------------
