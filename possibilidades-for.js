cadc65d5b06e697df1e8daf2795bd83e60ee34b2
Taitwelve <seuemail@exemplo.com>
2025-07-02 17:07:20 -0300

    fix: colocar um limitador para mostrar arrays

--- possibilidades-for.js
+++ possibilidades-for.js
@@ -4,10 +4,12 @@ for (let i = numeros.length - 1; i >= 0; i--) {
 }
 
 const numero = [100, 200, 300, 400, 500, 600];
- 
 for (let i = numero.length - 1; i >= 0; i -= 2) {
-  console.log(numero[i]);
+    console.log(numero[i]);
 }
 
-
+const numeroz = [100, 200, 300, 400, 500, 600];
+for (let i = numeroz.length - 1; numeroz[i] >= 300; i -= 2) {
+    console.log(numeroz[i]);
+}
 
