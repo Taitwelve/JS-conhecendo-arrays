8fdeffb2f46e49b822697383642a618b5d9d6e09
Taitwelve <seuemail@exemplo.com>
2025-07-02 17:19:15 -0300

    fix: exercícios para a prática de formas para manipulação de um array

---
+++ exercicios-finais.js
@@ -0,0 +1,64 @@
+// Escreva um código que utilize o loop for/of para iterar e imprimir cada elemento de um array.
+const numeros = [1, 2, 3, 4, 5];
+for (const numero of numeros) {
+    console.log(numero);
+}
+
+//Crie uma função que receba uma array e imprima no console o número do índice e o elemento.
+function imprimirIndicesEElementos(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`Índice: ${i}, Elemento: ${array[i]}`);
+    }
+}
+imprimirIndicesEElementos(['a', 'b', 'c', 'd']);
+
+//Crie uma função que receba uma array de números inteiros e retorne a soma dos elementos.
+function somarElementos(array) {
+    let soma = 0;
+    for (const numero of array) {
+        soma += numero;
+    }
+    return soma;
+}
+const numerosInteiros = [1, 2, 3, 4, 5];
+const resultadoSoma = somarElementos(numerosInteiros);
+console.log(`Soma dos elementos: ${resultadoSoma}`);
+
+
+//Crie uma função que receba uma array de números inteiros e retorne o menor valor e o maior valor da array, no seguinte formato: 'o menor número é X e o maior número é Y'.
+function encontrarMenorEMaior(array) {
+    let menor = array[0];
+    let maior = array[0];
+    
+    for (const numero of array) {
+        if (numero < menor) {
+            menor = numero;
+        }
+        if (numero > maior) {
+            maior = numero;
+        }
+    }
+    
+    return `O menor número é ${menor} e o maior número é ${maior}`;
+}
+const numerosInteiros2 = [10, 5, 8, 20, 15];
+const resultadoMenorMaior = encontrarMenorEMaior(numerosInteiros2);
+console.log(resultadoMenorMaior);
+
+//Crie um programa que utilize um laço for para percorrer uma array const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14] e exibir no console apenas os números pares contidos nesse array.
+const numeroz = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
+for (const numero of numeroz) {
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
+
+//Crie um programa que calcule a média dos números presentes em um array utilizando um loop for.
+const numerosParaMedia = [10, 20, 30, 40, 50];
+let somaMedia = 0;
+for (const numero of numerosParaMedia) {
+    somaMedia += numero;
+}
+const media = somaMedia / numerosParaMedia.length;
+console.log(`A média dos números é: ${media}`);
+
