8afbdea5a8f4181841ed8ed7aac2a76fcaf63d7e
Taitwelve <seuemail@exemplo.com>
2025-07-02 19:45:02 -0300

    fix: exemplo com função fora do forEach

--- foreach.js
+++ foreach.js
@@ -1,9 +1,15 @@
 const notas = [10, 6.5, 8, 7.5];
 let somaDasNotas = 0;
 
-notas.forEach(function(nota) {
+// notas.forEach(function(nota) {
+//   somaDasNotas += nota;
+// });
+
+notas.forEach (somaNotas);
+function somaNotas(nota) {
   somaDasNotas += nota;
-});
+}   
+
 const media = somaDasNotas / notas.length;
 console.log(`A média das notas é ${media}`);
 console.log(`A soma das notas é ${somaDasNotas}`);
