913d7a238dfa271731b0f019e211ebd0d7e6875f
Taitwelve <seuemail@exemplo.com>
2025-07-02 20:07:21 -0300

    fix: aplicando operador ternário para criar a regra de negócio pra limitar a nota a 10

--- map.js
+++ map.js
@@ -2,5 +2,5 @@ const notas = [5.5, 6.5, 10, 9.5, 8.0];
 // const notasAtualizadas = notas.map(function (nota) {
 //   return nota + 1;
 // });
-const notasAtualizadas = notas.map(nota => nota + 1); //Usar parenteses nos parâmetros é opcional quando há apenas um parâmetro
+const notasAtualizadas = notas.map(nota => nota + 1 >= 10 ? 10 : nota + 1); //Usar parenteses nos parâmetros é opcional quando há apenas um parâmetro
 console.log(notasAtualizadas);  
