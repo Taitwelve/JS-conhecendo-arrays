d7c051d5b99cd810dd9593f4ce42cbcf67805420
Taitwelve <seuemail@exemplo.com>
2025-07-02 17:05:29 -0300

    fix: Diminuindo de 2 em dois arrays

--- possibilidades-for.js
+++ possibilidades-for.js
@@ -3,5 +3,11 @@ for (let i = numeros.length - 1; i >= 0; i--) {
     console.log(numeros[i]);
 }
 
+const numero = [100, 200, 300, 400, 500, 600];
+ 
+for (let i = numero.length - 1; i >= 0; i -= 2) {
+  console.log(numero[i]);
+}
+
 
 
