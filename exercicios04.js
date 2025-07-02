bf080b7cbfb016a49d43a0f3b3b53c7d6d333f95
Taitwelve <seuemail@exemplo.com>
2025-07-02 19:49:07 -0300

    fix: exercícios para fixar

---
+++ exercicios04.js
@@ -0,0 +1,12 @@
+const notas = [5.5, 6.5, 10, 9.5];
+notas.forEach(function (nota) {
+  console.log(nota);
+});
+
+//-----------------------------------------------------------------
+
+const lista = [1, 2, 3, 4, 5];
+
+for (let indice = 0; indice < lista.length; indice++) {
+  console.log(lista[indice]);
+}
