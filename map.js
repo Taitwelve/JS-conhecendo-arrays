39b47e72943aced9aef450214fd55a05b5153c08
Taitwelve <seuemail@exemplo.com>
2025-07-02 20:03:38 -0300

    fix: fazendo o mesmo exemplo usando arrow function

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 const notas = [5.5, 6.5, 10, 9.5, 8.0];
-const notasAtualizadas = notas.map(function (nota) {
-  return nota + 1;
-});
+// const notasAtualizadas = notas.map(function (nota) {
+//   return nota + 1;
+// });
+const notasAtualizadas = notas.map((nota) => nota + 1);
 console.log(notasAtualizadas);  
