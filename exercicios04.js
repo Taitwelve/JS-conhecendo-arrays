db92fd31f867c545158cca5271114f86e5795608
Taitwelve <seuemail@exemplo.com>
2025-07-02 20:28:09 -0300

    fix: exercicio de fixação

--- exercicios04.js
+++ exercicios04.js
@@ -10,3 +10,14 @@ const lista = [1, 2, 3, 4, 5];
 for (let indice = 0; indice < lista.length; indice++) {
   console.log(lista[indice]);
 }
+
+//-----------------------------------------------------------------
+
+const numeros = [2, 4, 6, 8];
+let soma = 0;
+
+numeros.forEach(numero => {
+    soma += numero;
+});
+
+console.log("A soma dos números é:", soma);
