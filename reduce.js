4dafbc349abaa4dfadbc186f1a10ace9ee1e73b5
Taitwelve <seuemail@exemplo.com>
2025-07-04 13:18:52 -0300

    fix: exemplo mais simples e mais comum

--- reduce.js
+++ reduce.js
@@ -3,9 +3,10 @@ const salaJava = [6, 5, 8, 9, 5, 6];
 const salaPython = [7, 3.5, 8, 9.5];
 
 function calculaMedia(listaDeNotas) {
-  const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
-    return acumulador + nota;
-  }, 0);
+//   const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
+//     return acumulador + nota;
+//   }, 0);
+const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => acumulador + nota, 0);
 
     const media = somaDasNotas / listaDeNotas.length;
     return media;
