ecc9b8991bdb7a5ff5e0e9dea2247429c9d770af
Taitwelve <seuemail@exemplo.com>
2025-07-03 21:48:57 -0300

    fix: exercicios finais de capítulo

--- exercicios-finais.js
+++ exercicios-finais.js
@@ -62,3 +62,51 @@ for (const numero of numerosParaMedia) {
 const media = somaMedia / numerosParaMedia.length;
 console.log(`A média dos números é: ${media}`);
 
+
+//Utilize o método forEach para imprimir cada elemento de um array juntamente com seu índice.
+const frutas = ['maçã', 'banana', 'laranja', 'uva'];
+frutas.forEach((fruta, indice) => {
+    console.log(`Índice: ${indice}, Fruta: ${fruta}`);
+});
+
+//Você recebeu um array numeros contendo valores numéricos. Crie um programa que verifique se um número específico está presente nesse array. Se estiver, o programa deve retornar a posição (índice) desse número. Caso contrário, se o número não estiver presente, o programa deve retornar "-1".
+const numero = [5, 10, 15, 20, 25];
+const numeroProcurado = 25;
+const indice = numero.indexOf(numeroProcurado);
+if (indice !== -1) {
+    console.log(`O número ${numeroProcurado} está presente no índice: ${indice}`);
+}else {
+    console.log('-1');
+}   
+
+//__________________________________________________________________________________________________________
+
+const nomesTurmaA = ['João Silva','Maria Santos', 'Pedro Almeida'];
+const nomesTurmaB = ['Carlos Oliveira','Ana Souza','Lucas Fernandes'];
+
+const todasAsTurmas = nomesTurmaA.concat(nomesTurmaB);
+const alunoProcurado = 'Ana Soa';
+const alunoEncontrado = todasAsTurmas.find(aluno => aluno === alunoProcurado);
+if (alunoEncontrado) {
+    console.log(`Aluno(a) encontrado(a): ${alunoEncontrado}`);
+}
+else {
+    console.log('Aluno(a) não encontrado(a)');
+}
+
+
+//__________________________________________________________________________________________________________
+
+const numerosInt = [6, 9, 12, 15, 18, 21];
+//Utilize o método forEach() para multiplicar cada elemento do array por 3 e exibir no console o resultado de cada multiplicação. Depois, utilize o método findIndex() para encontrar o índice do número 18 no array original.
+numerosInt.forEach((numero) => {
+    console.log(numero * 3);
+});
+const indiceNumero18 = numerosInt.findIndex(numero => numero === 18);   
+if (indiceNumero18 !== -1) {
+    console.log(`O índice do número 18 é: ${indiceNumero18}`);
+} else {
+    console.log('Número 18 não encontrado no array.');
+}
+
+//__________________________________________________________________________________________________________
