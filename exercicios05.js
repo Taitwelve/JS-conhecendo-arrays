6fa4c92aad34d065f37d9ef9f56ecc7cf56894bf
Taitwelve <seuemail@exemplo.com>
2025-07-04 13:54:35 -0300

    fix: exercicio de reduce

--- exercicios05.js
+++ exercicios05.js
@@ -2,3 +2,9 @@ const notas = [10, 5, 7, 4, 8, 3];
 
 const aprovados = notas.filter((nota) => nota >= 7);
 console.log(aprovados);
+
+//_____________________________________________________________________________________________
+
+const numeros = [43, 50, 65, 12]
+const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+console.log(soma)
