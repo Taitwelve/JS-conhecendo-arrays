3c3c757f33ddc23b10e2839361b15a0d7b7f2761
Taitwelve <seuemail@exemplo.com>
2025-07-08 18:55:43 -0300

    fix: usando push que clona os arrays, o que eu quero e o que está referenciado

---
+++ clonar.js
@@ -0,0 +1,11 @@
+const notas = [7, 8, 9, 10];
+const novaListaNotas = notas;
+
+novaListaNotas.push(10);
+// let nota = 9.75;
+// let novasNotas = nota;
+
+// nota = 8.5; 
+
+console.log(notas); 
+console.log(novaListaNotas);
