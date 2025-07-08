7f67916ff470656744806aec972120d0e5cd6042
Taitwelve <seuemail@exemplo.com>
2025-07-08 19:01:58 -0300

    fix: exemplo de atribuição por valor

--- clonar.js
+++ clonar.js
@@ -10,3 +10,13 @@ const novaListaNotas = [...notas, 10];
 
 console.log(notas); 
 console.log(novaListaNotas);
+
+//_______________________________________________________________________
+
+let num1 = 10;
+let num2 = num1;
+
+num2 += 5;
+num1 += 1;
+
+console.log(`Num1 é ${num1}. Num2 é ${num2}`);
