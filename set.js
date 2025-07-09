e750ff173344b2462c643be02af14b154f01f73b
Taitwelve <seuemail@exemplo.com>
2025-07-09 20:49:34 -0300

    fix: outra forma de como fazer um set virar um array

--- set.js
+++ set.js
@@ -1,6 +1,8 @@
 const nomes = ['Ana', 'Bia', 'Carlos', 'Daniel', 'Daniel','Eduardo', 'Fernanda', 'Gustavo', 'Helena', 'Igor','Fernanda', 'Joana'];
 
 const nomesAtualizados = new Set(nomes);
-const listaNomesAtualizados = [...nomesAtualizados]
+// const listaNomesAtualizados = [...nomesAtualizados]
+const listaNomesAtualizados = [...new Set(nomes)];
+
 console.log(nomesAtualizados);
 console.log(listaNomesAtualizados);
