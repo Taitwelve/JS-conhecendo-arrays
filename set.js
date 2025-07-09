942459c9566a09418f62408aae5406351e016bee
Taitwelve <seuemail@exemplo.com>
2025-07-09 20:46:15 -0300

    fix: forçando um erro ao adicionar informações no set, porque ele parece um array, mas não é um array

--- set.js
+++ set.js
@@ -1,4 +1,5 @@
 const nomes = ['Ana', 'Bia', 'Carlos', 'Daniel', 'Daniel','Eduardo', 'Fernanda', 'Gustavo', 'Helena', 'Igor','Fernanda', 'Joana'];
 
 const nomesAtualizados = new Set(nomes);
+nomesAtualizados.push('João');
 console.log(nomesAtualizados);
